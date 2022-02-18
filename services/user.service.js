cd194d438339f3a4b1fd054660fc1d5a705881ac
Miguel Angel Muñoz Pozos <miguel.pozos@wagento.com>
2022-02-17 18:39:43 -0600

    Integración de node-postgres

--- services/user.service.js
+++ services/user.service.js
@@ -1,14 +1,18 @@
 const boom = require('@hapi/boom');
+const getConnection = require('../libs/postgres');
+
 
 class UserService {
-  constructor() {}
+  constructor() { }
 
   async create(data) {
     return data;
   }
 
   async find() {
-    return [];
+    const client = await getConnection();
+    const rta = await client.query('SELECT * FROM task');
+    return rta.rows;
   }
 
   async findOne(id) {

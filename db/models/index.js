37529b47d1329c33fa195246af45d74dd0978a4b
Miguel Angel Muñoz Pozos <miguel.pozos@wagento.com>
2022-02-25 13:59:50 -0600

    Resolviendo las relaciones uno a uno

--- db/models/index.js
+++ db/models/index.js
@@ -5,6 +5,10 @@ function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
   Customer.init(CustomerSchema, Customer.config(sequelize));
 
+  User.associate(sequelize.models);
+  Customer.associate(sequelize.models);
+
+
 }
 
 module.exports = setupModels;
